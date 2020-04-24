a514e2da9fec345f21a95b8a286a7326a6df9df0
ramdane.merakeb <ramdane.merakeb@alumni.univ-avignon.fr>
2020-04-25 00:13:33 +0200

    premiere partie

--- MopionJFX/src/com/MorpionJFX/Main.java
+++ MopionJFX/src/com/MorpionJFX/Main.java
@@ -1,19 +1,21 @@
 package com.MorpionJFX;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("ressources/application.css").toExternalForm());
+			Parent root = FXMLLoader.load(getClass().getResource("vue/Vue.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setTitle("Morpion Champion");
 			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
--- MopionJFX/src/com/MorpionJFX/controleur/MainController.java
+++ MopionJFX/src/com/MorpionJFX/controleur/MainController.java
@@ -1,5 +1,67 @@
 package com.MorpionJFX.controleur;
 
-public class MainController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class MainController implements Initializable {
+	
+	@FXML 
+	private Button nvPartie;
+	
+	@FXML 
+	private Button ldPartie;
+	
+	
+	@FXML
+	private void lancerPartie(ActionEvent e){
+	    exitWindows();
+	    openWindows();
+	}
+	
+	@FXML
+	private void loadPartie(ActionEvent e){
+	    exitWindows();
+	    openWindows();
+	}
+	
+	private void exitWindows() {
+		// on recupere la scene courrante puis la fermer
+	    Stage stage = (Stage) nvPartie.getScene().getWindow();
+	    stage.close();
+	}
+	
+	private void openWindows() {
+		Stage primaryStage = new Stage();
+	    Parent root = null;
+	    try {
+	        root = FXMLLoader.load(getClass().getResource("../vue/VueJeu.fxml"));
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	    primaryStage.setTitle("2 em fenetre");
+	    primaryStage.setScene(new Scene(root, 600, 400));
+	    primaryStage.show();
+	}
+	
+	
+	
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		System.out.println("hello");
+		
+	}
+	
+	
 
 }
